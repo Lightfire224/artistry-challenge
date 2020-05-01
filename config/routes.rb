5e3ad3dd910d11fa8fef3389cb56dfa06c07f80a
lightfire224 <elias.taveras@columbia.edu>
2020-05-01 01:55:29 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :artist_instruments, only: [:new, :create]
+  resources :artists, only: [:index, :new, :show, :create]
+  resources :instruments, only: [:index]
 end
